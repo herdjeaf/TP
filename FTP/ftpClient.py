de34635f378db1e330b188fd3913069fc84f4caf
herdjeaf <herdjeafoua@gmail.com>
2023-07-30 08:31:32 +0100

    New updates

--- FTP/ftpClient.py
+++ FTP/ftpClient.py
@@ -1,14 +1,30 @@
-from ftplib import FTP_TLS
-# Informations de connexion au serveur FTP
-ftp_server = '127.0.0.1'
-username = 'herdjeaf'
-password = '1234'
-# Établir une connexion avec le serveur FTP
-ftp = FTP_TLS(ftp_server)
-ftp.login(username, password)
+import ftplib
+import ssl
+
+# Paramètres de connexion FTP
+ftp_host = 'localhost'
+ftp_port = 21
+ftp_username = 'username'
+ftp_password = 'password'
+
+# Chemin d'accès à la clé privée et au certificat
+keyfile = 'D:/Cours/M2/IoT/TP/OpenSSL/certificate/server.key'
+certfile = 'D:/Cours/M2/IoT/TP/OpenSSL/certificate/server.pem'
+
+# Créer un contexte SSL/TLS personnalisé
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+
+# Créer une connexion FTP sécurisée
+ftp = ftplib.FTP_TLS()
+ftp.connect(ftp_host, ftp_port)
+ftp.auth()
+ftp.prot_p()
+ftp.login(user=ftp_username, passwd=ftp_password)
+
+# Effectuer des opérations FTP sécurisées
 # Liste les fichiers/dossiers dans le répertoire distant
 ftp.cwd('')
-ftp.prot_p()
 ftp.retrlines('LIST')
 files = ftp.nlst()
 # Affiche la liste des fichiers/dossiers
@@ -26,6 +42,5 @@ ftp.retrbinary('RETR ' + filename, open(local_filepath, 'wb').write)
 local_filepath = 'D:/Cours/M2/IoT/TP/TCP/tcpClient.py'
 filename = 'tcpClient.py'
 ftp.storbinary('STOR ' + filename, open(local_filepath, 'rb'))
-
 # Fermer la connexion FTP
 ftp.quit()
--- UDP/udpServer.py
+++ UDP/udpServer.py
@@ -1,5 +1,9 @@
 import socket
-import ssl
+import time
+
+#Configuration du débit de données maximum en octets par seconde
+MAX_BYTES_PER_SECOND = 1000
+
 
 # Créer un socket UDP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,22 +12,32 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 8888) # Utilisez l'adresse IP de votre choix et un port disponible
 server_socket.bind(server_address)
 
+# Initialiser le temps de départ et le nombre d'octets reçus
+start_time = time.time()
+bytes_received = 0
+
 # Boucle principale du serveur pour l'échange des données 
 while True:
+
     print('Le serveur est en attente de messages...')
+    # Recevoir des données depuis le socket
+    data = server_socket.recvfrom(1024)
+    bytes_received += len(data)
 
-    # Recevoir les données et l'adresse du client
-    data = server_socket.recvfrom(4096) # 1024 est la taille maximale des données à recevoir
+    # Calculer le temps écoulé depuis le début du transfert
+    elapsed_time = time.time() - start_time
 
+    # Vérifier si le débit de données maximum a été atteint
+    if bytes_received > MAX_BYTES_PER_SECOND:
+        # Attendre suffisamment de temps pour respecter le débit de données maximum
+        time_to_wait = bytes_received / MAX_BYTES_PER_SECOND - elapsed_time
+        time.sleep(time_to_wait)
+
+    # Traiter les données reçues
     # Afficher les informations du client
     print(f"Message reçu: {data[0]}")
     print(f"De: {data[1]}")
-# Traiter les données reçues (vous pouvez ajouter votre propre logique ici)
-# ...
-    break
-# enveloppe le socket avec SSL pour créer une connexion sécurisée
-
-# Envoyer une réponse au client
-response = "Message reçu avec succès!"
-server_socket.sendto(response.encode(), data[1])
-server_socket.close()
+    # Envoyer une réponse au client
+    response = "Message reçu avec succès!"
+    server_socket.sendto(response.encode(), data[1])
+    server_socket.close()
